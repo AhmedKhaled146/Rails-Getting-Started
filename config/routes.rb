bdc2342b2eae1e09ce80b448bbb6b1b6303bc583
AhmedKhaled146 <a7med74yaso@gmail.com>
2025-11-09 16:16:31 +0200

    action mailer is done

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,10 @@ Rails.application.routes.draw do
   # delete "/products/:id", to: "products#destroy"
 
   # ############################################################################
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+  resource :unsubscribe, only: [ :show ]
   # ############################################################################
 
 
